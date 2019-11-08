499e9c0d5dc868a4aaa3f2307137546a248e0697
Kelby Mittan <kelbymittan@pursuit.org>
2019-11-08 09:53:17 -0500

    answered a few questions

---
+++ Autolayout-Lab/Autolayout-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout-Lab
+//
+//  Created by Kelby Mittan on 11/7/19.
+//  Copyright © 2019 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
